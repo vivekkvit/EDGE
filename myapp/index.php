10c458db8aeb4d8479e13bcd5d783ff4d8fd8689
Vivek Jain <vivekja@cybage.com>
2020-08-21 09:23:13 +0530

    Testing changes - vjain

--- myapp/index.php
+++ myapp/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <p>
-            Hello Team, Today is <?= date('l \t\h\e jS') ?>.
+            Hello EDGE Team, Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
 </html>
